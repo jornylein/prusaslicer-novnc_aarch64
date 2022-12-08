92b5c1761aba3f53e9288f5db52f64d3c4d431d8
jornylein <joern8fl3@web.de>
2022-12-08 05:31:59 +0100

    some corrections

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
   
-source /docker_build.sh
+./docker_build.sh
 
-docker run --detach --volume=prusaslicer-novnc-data:/configs/ --volume=prusaslicer-novnc-prints:/prints/ -p 8234:8080 -e SSL_CERT_FILE="/etc/ssl/certs/ca-certificates.crt" --name=prusaslicer-novnc prusaslicer-novnc_aarch64:latest
+docker run --detach --volume=prusaslicer-novnc-data:/configs/ --volume=prusaslicer-novnc-prints:/prints/ -p 8080:8080 -e SSL_CERT_FILE="/etc/ssl/certs/ca-certificates.crt" --name=prusaslicer-novnc prusaslicer-novnc_aarch64:latest
