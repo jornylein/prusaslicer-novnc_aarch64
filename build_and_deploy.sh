7107e50da4c65d7c1123f2ded5a679d8d0bf9b46
jornylein <joern8fl3@web.de>
2022-12-12 01:30:47 +0100

    volumes added

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -2,4 +2,6 @@
   
 ./docker_build.sh
 
+docker volume create prusaslicer-novnc-data
+docker volume create prusaslicer-novnc-prints
 docker run --detach --volume=prusaslicer-novnc-data:/configs/ --volume=prusaslicer-novnc-prints:/prints/ -p 8080:8080 -e SSL_CERT_FILE="/etc/ssl/certs/ca-certificates.crt" --name=prusaslicer-novnc prusaslicer-novnc_aarch64:latest
